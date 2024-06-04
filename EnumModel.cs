dc3c4cd3b86bd96367fa989fba4c9b1c0251b792
Ali Youssef <ayoussef@vanrise.com>
2024-06-04 17:34:20 +0300

    Added EnumModel

---
+++ EnumModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.BusinessProcess.Web.Models
+{
+    public class EnumModel
+    {
+        public int Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
